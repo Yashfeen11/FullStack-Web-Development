75ebcd02278a3af68edd6d466a1a019f55eb9660
Yashfeen11 <yashfeen6343@gmail.com>
2026-07-22 22:29:20 +0530

    Initial commit

---
+++ Web-Dev-Practice/JavaScript_Practice/02-TriviaBot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="MyBot";
+let botLocation="Moradabad";
+let favoriteLanguage="Java";
+console.log("My name is " +(botName)+ " and I live on "+(botLocation)+".");
+console.log("My favorite programming language is "+(favoriteLanguage)+ ".");
+let codingFact="FunFact in " +(favoriteLanguage)+ ".";
+console.log(codingFact);
+codingFact="New FunFact in " +(favoriteLanguage)+ ".";
+console.log(codingFact);
+codingFact= "Again Funfact in " +(favoriteLanguage)+ ".";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - "+(botName)+ " from " +(botLocation)+ ".");
---
+++ Web-Dev-Practice/JavaScript_Practice/03-Sentencemaker.js
@@ -0,0 +1,16 @@
+let adjective="awesome";
+let noun="Monkey";
+let verb="climbing";
+let place="jungle"
+let adjective2="running";
+let noun2="Bananas";
+let firstStory= "Once upon a time, there was a(n) "+ [adjective] +" " +[noun] + " who loved to eat " + [noun2] + ". The "+[noun]+ " lived in a "+[place]+ " and had " +[adjective2]+ " nostrils that blew fire when it was " +[verb]+ ".";
+console.log("First story: "+firstStory);
+adjective="young";
+noun="girrafe";
+verb="eating";
+place="field"
+adjective2="sleeping";
+noun2="elephant";
+let secondStory= "Once upon a time, there was a(n) "+ [adjective] +" " +[noun] + " who loved to eat " + [noun2] + ". The "+[noun]+ " lived in a "+[place]+ " and had " +[adjective2]+ " nostrils that blew fire when it was " +[verb]+ ".";
+console.log("Second story: " +secondStory);
