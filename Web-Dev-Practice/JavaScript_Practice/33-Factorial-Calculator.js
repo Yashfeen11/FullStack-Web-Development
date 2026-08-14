ec11634563d6cc23a9a8f6a07b00831c0d7a5651
Yashfeen11 <yashfeen6343@gmail.com>
2026-08-14 23:55:28 +0530

    Factorial Calculator

---
+++ Web-Dev-Practice/JavaScript_Practice/33-Factorial-Calculator.js
@@ -0,0 +1,11 @@
+let num =4;
+function factorialCalculator(num){
+  let result = 1;
+  for( let i =1 ; i<=num; i++){
+    result = i*result;
+  }
+  return result;
+}
+let factorial = factorialCalculator(num);
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
