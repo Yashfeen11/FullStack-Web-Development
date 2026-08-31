64a88c83674be8418688335f6f5da5bdda15f579
Yashfeen11 <yashfeen6343@gmail.com>
2026-08-31 22:42:31 +0530

    Lcm of numbers using for loop and while loop

--- Web-Dev-Practice/JavaScript_Practice/64-Range-Based-LCM-Calculator.js
+++ Web-Dev-Practice/JavaScript_Practice/64-Range-Based-LCM-Calculator.js
@@ -25,3 +25,23 @@ function smallestCommons(arr) {
 console.log(smallestCommons([1, 5]));   // 60
 console.log(smallestCommons([5, 1]));   // 60
 console.log(smallestCommons([2, 10]));  // 2520
+
+/* function smallestCommons(arr){
+let min= Math.min(arr[0],arr[1]);
+let max= Math.max(arr[0], arr[1]);
+let mul = 1;
+for(let k=min; k<=max; k++){
+  mul= mul*k
+}
+
+for(let i = 1; i<=mul; i++){
+    for(let j=min; j<=max; j++){
+      if(i%j==0){
+        
+      }
+    }
+}
+
+}
+smallestCommons([1, 5])
+*/
