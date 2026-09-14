4764bccb0e3d93ed767d4e55a09f191517d330d7
Yashfeen11 <yashfeen6343@gmail.com>
2026-09-14 20:07:22 +0530

    Is it a palindrome using regex

--- Web-Dev-Practice/JavaScript-Part2_Practice/12-Palindrome-Checker/script.js
+++ Web-Dev-Practice/JavaScript-Part2_Practice/12-Palindrome-Checker/script.js
@@ -1,4 +1,27 @@
-let button= document.querySelector("#check-btn");
-button.addEventListener("click", ()=>{
-  alert("Plese input a value");
-})
+let button = document.querySelector("#check-btn");
+let textInput = document.querySelector("#text-input");
+const result = document.querySelector("#result");
+
+button.addEventListener("click", () => {
+  palindromeCheck(textInput.value);
+});
+
+function palindromeCheck(text) {
+ 
+  const originalText = text;
+  const cleanText = originalText.replace(/[^a-zA-Z0-9]/g, "");
+
+  let txt = cleanText.toLowerCase();
+  let rev = txt.split("").reverse().join("");
+
+  if (txt === "") {
+    alert("Please input a value");
+    return;
+  }
+
+  if (txt === rev) {
+    result.innerHTML = `<span class="input-text">${originalText}</span> is a palindrome`;
+  } else {
+    result.innerHTML = `<span class="input-text">${originalText}</span> is not a palindrome`;
+  }
+}
